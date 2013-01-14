22254c2d8dd7b7daeab2347280b7e592cdc92e7c
Rodrigo Morgado <rodrigo@ewok.cl>
2013-01-14 12:37:37 -0300

    Nuevos cambios a la aplicacion, trabajando en devise para configurar los perfiles de usuarios

--- db/migrate/20130112214030_devise_create_users.rb
+++ db/migrate/20130112214030_devise_create_users.rb
@@ -1,6 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+      
       t.string :first_name
       t.string :last_name
       t.string :profile_name
